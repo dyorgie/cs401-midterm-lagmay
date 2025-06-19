7c7c572c333e10f725eda55e259974f2f5e24c40
Jethro Lagmay <george.lagmay113@gmail.com>
2025-06-19 12:11:59 +0800

    updated models

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -5,10 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Course extends Model
 {
     public function teacher(): BelongsTo {
         return $this->belongsTo(Teacher::class);
+    }
+
+    public function student(): HasMany {
+        return $this->hasMany(Student::class);
+    }
+
+    public function schedule(): HasMany {
+        return $this->hasMany(Schedule::class);
     }
 }
--- app/Models/Student.php
+++ app/Models/Student.php
@@ -4,11 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Student extends Model
 {
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function course(): HasMany {
+        return $this->hasMany(Course::class,'course_id');
     }
 }
